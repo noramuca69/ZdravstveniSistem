4c7211e901275873539317e6183a9a137254660d
noramuca69 <marc.marko1@gmail.com>
2025-05-04 19:51:33 +0200

    aaaa

---
+++ ZdravstveniSistem/Migrations/202505041507400_safsa.cs
@@ -0,0 +1,18 @@
+﻿namespace ZdravstveniSistem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class safsa : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Obisks", "DatumObiska", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Obisks", "DatumObiska", c => c.DateTime(nullable: false));
+        }
+    }
+}
---
+++ ZdravstveniSistem/Migrations/202505041509560_sfasfasa.cs
@@ -0,0 +1,20 @@
+﻿namespace ZdravstveniSistem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sfasfasa : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Obisks", "DatumObiska", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Pacients", "DatumRojstva", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Pacients", "DatumRojstva", c => c.String());
+            AlterColumn("dbo.Obisks", "DatumObiska", c => c.String());
+        }
+    }
+}
